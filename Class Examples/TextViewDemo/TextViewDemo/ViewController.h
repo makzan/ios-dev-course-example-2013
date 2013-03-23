86b49f501cf73e9de92780a6ff51116a3b845899
Thomas Seng Hin Mak <makzan@gmail.com>
2013-03-23 21:56:41 +0800

    Added RGB slider example
    Signed-off-by: Thomas Seng Hin Mak <makzan@gmail.com>

---
+++ Class Examples/TextViewDemo/TextViewDemo/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  TextViewDemo
+//
+//  Created by Seng Hin Mak on 23/3/13.
+//  Copyright (c) 2013 42games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITextViewDelegate>
+@property (weak, nonatomic) IBOutlet UITextView *feedbackTextView;
+
+
+
+@end
